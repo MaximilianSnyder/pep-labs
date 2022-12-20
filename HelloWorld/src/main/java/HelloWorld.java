b6c2f2eec6165b18aa2eca0b24cb2662c8954447
MaximilianSnyder <maxxxsnyderr@gmail.com>
2022-12-20 17:38:08 +0000

    Saved progress on MethodParameters 2022-12-20 17:38:08 +0000

--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
      * You can use the test results to check for differences.
      * @return "Hello, world!"
      */
-    public String sayHello(String text){
-        return "Hello World";
+    public String sayHello(){
+        return "Hello, world!";
     }
 }
