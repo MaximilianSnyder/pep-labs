65bad84ce45f5ef26e5907b5a2ee26d0b32f0c6c
MaximilianSnyder <maxxxsnyderr@gmail.com>
2022-12-26 19:51:59 +0000

    Saved progress on MethodParameters 2022-12-26 19:51:59 +0000

--- GreaterOrLesser/src/main/java/GreaterOrLesser.java
+++ GreaterOrLesser/src/main/java/GreaterOrLesser.java
@@ -11,10 +11,8 @@ public class GreaterOrLesser {
      */
     public String decide(int a, int b){
         if (a == b);
-        return "Equal";
-    }
-    public String decide(int a, int b){
+        return "Equal";}
         if (a > b);
-        return "Greater";
-    }
+        return 
+    
 }
