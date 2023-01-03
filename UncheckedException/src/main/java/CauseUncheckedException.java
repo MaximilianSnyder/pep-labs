1afe7d5876e63e0d43b908c5f95b07a565611775
MaximilianSnyder <maxxxsnyderr@gmail.com>
2023-01-03 22:35:09 +0000

    Saved progress on Lists 2023-01-03 22:35:09 +0000

--- UncheckedException/src/main/java/CauseUncheckedException.java
+++ UncheckedException/src/main/java/CauseUncheckedException.java
@@ -13,7 +13,7 @@ public class CauseUncheckedException {
      * Notice that errors are separate from exceptions. Errors are external to Java and can occur when some external
      * event happens, like running out of computer memory.
      */
-    public void throwUncheckedException(){
-
+    public void throwUncheckedException()throws UncheckedException{
+throw new UncheckedException;
     }
 }
