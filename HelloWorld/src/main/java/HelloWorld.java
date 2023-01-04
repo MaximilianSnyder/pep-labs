3785ef1ba48ef55391e63356208138424b24408f
MaximilianSnyder <maxxxsnyderr@gmail.com>
2023-01-04 22:40:42 +0000

    Saved progress on Lists 2023-01-04 22:40:42 +0000

--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -6,8 +6,7 @@ public class HelloWorld {
      * You can use the test results to check for differences.
      * @return "Hello, world!"
      */
-    public String sayHello(){
+    public static void sayHello(){
         System.out.println("Hello, world!");
-        return "Hello, world!";
     }
 }
