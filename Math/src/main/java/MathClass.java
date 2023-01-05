8940b39d034ba8a78e0dd668deb20189a55c4a6f
MaximilianSnyder <maxxxsnyderr@gmail.com>
2023-01-05 22:39:07 +0000

    Saved progress on Map 2023-01-05 22:39:07 +0000

--- Math/src/main/java/MathClass.java
+++ Math/src/main/java/MathClass.java
@@ -20,6 +20,6 @@ public class MathClass {
      * @return return a^b using the pow() method of the Math class.
      */
     public double mathPow(double a, double b){
-        return;
+        return double;
     }
 }
