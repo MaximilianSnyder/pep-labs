b3627cbdb8be00b530f490538c81df84f1ad62a0
MaximilianSnyder <maxxxsnyderr@gmail.com>
2023-01-13 23:10:55 +0000

    Saved progress on InnerJoin 2023-01-13 23:10:55 +0000

--- Map/src/main/java/MapExercise.java
+++ Map/src/main/java/MapExercise.java
@@ -43,8 +43,8 @@ public class MapExercise {
      */
     public int getSize(Map<Integer,String> map){
         //        return total number of key/value pairs in the map
-       int len = map.size();
-       return len;
+       int size = map.size();
+       return size;
     }
 
     /**
