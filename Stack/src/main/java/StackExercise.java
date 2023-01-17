be022d8d87dbfd8ba886ac2e417d8dd4d3d9351e
MaximilianSnyder <maxxxsnyderr@gmail.com>
2023-01-17 18:32:12 +0000

    Saved progress on Stack 2023-01-17 18:32:12 +0000

--- Stack/src/main/java/StackExercise.java
+++ Stack/src/main/java/StackExercise.java
@@ -22,6 +22,7 @@ public class StackExercise {
      */
     public Deque<String> createNewStack(){
         ArrayDeque<String>stack= new ArrayDeque<String>();
+        return stack;
     }
 
     /**
@@ -29,7 +30,7 @@ public class StackExercise {
      * @return the number of elements in the deque.
      */
     public int getSize(Deque<String> stack){
-        return 0;
+        return stack.size();
     }
 
     /**
@@ -38,6 +39,7 @@ public class StackExercise {
      * @param item a String to be added to the deque.
      */
     public void addToTopOfStack(Deque<String> stack, String item){
+        stack.push(item);
     }
 
     /**
@@ -47,7 +49,7 @@ public class StackExercise {
      * @return the value popped from the top of the deque.
      */
     public String removeFromTopOfStack(Deque<String> stack){
-        return "";
+        return stack.removeLast();
     }
     /**
      * Return the value at the top of a deque without removing the value.
@@ -55,6 +57,6 @@ public class StackExercise {
      * @return the value peeked from the top of the deque.
      */
     public String getTopOfStackWithoutRemoving(Deque<String> stack){
-        return "";
+        return stack.getLast();
     }
 }
