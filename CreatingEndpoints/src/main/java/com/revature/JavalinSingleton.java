1a3e76a66cfb4987584a489ed4cff2dcc0c0d049
MaximilianSnyder <maxxxsnyderr@gmail.com>
2023-01-18 22:33:29 +0000

    Saved progress on CreatingEndpoints 2023-01-18 22:33:29 +0000

--- CreatingEndpoints/src/main/java/com/revature/JavalinSingleton.java
+++ CreatingEndpoints/src/main/java/com/revature/JavalinSingleton.java
@@ -13,10 +13,10 @@ public class JavalinSingleton {
      */
     public static Javalin getInstance(){
         Javalin app = Javalin.create();
-        app.start(9000);
-        // HTTP GET REQUEST to http://localhost:9000/hello
+        
+        
     app.get("/hello", ctx -> {
-        //logic to be executed when this endpoint is hit
+        
         ctx.result("Hello World");
     });
 
