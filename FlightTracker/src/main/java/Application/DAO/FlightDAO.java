4be1a5947b72dccb12cdab54342b31b5d17b4063
MaximilianSnyder <maxxxsnyderr@gmail.com>
2023-01-20 19:00:03 +0000

    Saved progress on InnerJoin 2023-01-20 19:00:02 +0000

--- FlightTracker/src/main/java/Application/DAO/FlightDAO.java
+++ FlightTracker/src/main/java/Application/DAO/FlightDAO.java
@@ -147,11 +147,14 @@ public class FlightDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "UPDATE flight SET departure_city = ? , arrival_city = ? WHERE flight_id = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write PreparedStatement setString and setInt methods here.
-
+            // preparedStatement.setInt(1, id);
+            preparedStatement.setString(1, flight.departure_city);
+            preparedStatement.setString(2, flight.arrival_city);
+            preparedStatement.setInt(3, id);
 
             preparedStatement.executeUpdate();
         }catch(SQLException e){
