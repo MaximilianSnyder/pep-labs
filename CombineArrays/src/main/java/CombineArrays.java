f59e42eab275a6dd93d1f9c959546a5a0f2b619d
MaximilianSnyder <maxxxsnyderr@gmail.com>
2023-01-21 21:42:48 +0000

    Saved progress on AdvancedBooleans 2023-01-21 21:42:48 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -8,10 +8,11 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int[] array = new int[arr1.length = arr2.length];
         for(int i =0; i<arr1.length; i++){
             for(int j = 0; j<arr2.length; j++){
             }
         }
-        return arr1 ++ arr2;
+       return ;
     }
 }
