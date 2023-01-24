dae6fe5d66ae33a50ec4be66e5a8120462a1fead
MaximilianSnyder <maxxxsnyderr@gmail.com>
2023-01-24 01:23:19 +0000

    Saved progress on ContainsDuplicates 2023-01-24 01:23:19 +0000

--- InnerJoin/problem1.sql
+++ InnerJoin/problem1.sql
@@ -1,1 +1,1 @@
-SELECT student.id, student.student_name FROM class INNER JOIN student ON student.class_title = "Math";
+SELECT student.id FROM class INNER JOIN student ON class.class_title = student.class_title WHERE class.techer_name = 'Ms. Lovelace';
